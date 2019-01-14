b25f45ca13f1c9b8e967dcade299bcd24053cd07
sebsago <ssagodira@gmail.com>
2019-01-14 09:44:39 +0000

    allongement du sleep dans le main

--- partageable/main.cpp
+++ partageable/main.cpp
@@ -19,5 +19,5 @@ int main(int argc, char * argv)
 
 	std::cout << getComposant1Version() << std::endl;
 	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
-	Sleep(1000);
+	Sleep(10000);
 }
